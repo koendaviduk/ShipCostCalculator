08badc171c8ad05a773121d12dc8cc85142fb926
koendaviduk <davidukj@mail.uc.edu>
2023-09-24 22:43:17 -0400

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        double itemPrice = 0;
+        System.out.print("Enter your item price: ");
+
+        if (in.hasNextDouble())
+        {
+            itemPrice = in.nextDouble();
+            in.nextLine();
+            if (itemPrice >= 100)
+            {
+                double shippingPrice = 0;
+                double totalPrice = shippingPrice + itemPrice;
+                System.out.println("Your total shipping cost is: " + shippingPrice + " and your total price is: " + totalPrice);
+
+            }
+            else
+            {
+                double shippingPrice = itemPrice * 0.02;
+                double totalPrice = shippingPrice + itemPrice;
+                System.out.println("Your total shipping cost is: " + shippingPrice + " and your total price is: " + totalPrice);
+
+            }
+        }
+        else
+        {
+            System.out.println("Please run again and enter a valid amount!");
+        }
+
     }
 }
